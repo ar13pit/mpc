ec3ed1975fb9be9993f6292f0fbd3b74152ea97e
Arpit Aggarwal <ar13pit@gmail.com>
2018-03-11 17:59:46 +0100

    need to update based on mpcObj

--- mpcsolver.m
+++ mpcsolver.m
@@ -1,5 +1,17 @@
 function [u]=mpcsolver(mpcObj,options)
     
+if ~(isa(mpcObj,'mpcObj'))
+    error('First input argument must be an object of the "mpcObj" class.');
+end
+
+if nargin > 1
+    if ~(mpc_check('options',options))
+        error('Options not recognised. Use "mpcOptions" to set the correct options.')
+    end
+else
+    options = mpcOptions('default');
+end
+
 cost = options.Cost;
 predictFlag = options.PredictState;
 
