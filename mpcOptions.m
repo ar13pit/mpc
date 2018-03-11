15eb4f0f153ca46ccf1a0d92a1d1514c0367e838
Arpit Aggarwal <ar13pit@gmail.com>
2018-03-11 17:59:13 +0100

    created default options

--- mpcOptions.m
+++ mpcOptions.m
@@ -1,8 +1,30 @@
-function options = mpcOptions
+function options = mpcOptions(type)
 
-options = struct( ...
-    'Cost',[], ...
-    'PredictState',[], ...
-    'OptimizationType',[] ...
-);
+if nargin < 1
+    type = 'default';
+end
+
+switch type
+    case 'default'
+        options = MPCOptions();
+        options.Solver = 'QuadProg';
+        options.Cost = 'Quadratic';
+        options.PredictState = true;
+        options.Constraints = false;
+        options.TerminalCost = 'LQR';
+    
+    case 'possible'
+        options.Solver = {'QuadProg'};
+        options.Cost = {'Quadratic','Linear'};
+        options.PredictState = {true,false};
+        options.Constraints = {true,false};
+        options.TerminalCost = {'LQR','DLYAP','LMI'};
+        
+        
+    
+    otherwise
+        error('Invalid input argument')
+        
+end
+
 end
